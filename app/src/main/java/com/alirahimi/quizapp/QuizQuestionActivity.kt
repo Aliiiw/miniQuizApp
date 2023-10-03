a93a903d89482a6d0540a0864842cfef95e16438
Ali Rahimi <1aliirahimi1@gmail.com>
2023-10-03 14:56:19 +0330

    add the interface of question

--- app/src/main/java/com/alirahimi/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/alirahimi/quizapp/QuizQuestionActivity.kt
@@ -1,16 +1,52 @@
 package com.alirahimi.quizapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.alirahimi.quizapp.utils.Constants
 
 class QuizQuestionActivity : AppCompatActivity() {
+
+    private var progressBar: ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var tvQuestionTitle: TextView? = null
+    private var ivQuestionImage: ImageView? = null
+    private var tvOption1: TextView? = null
+    private var tvOption2: TextView? = null
+    private var tvOption3: TextView? = null
+    private var tvOption4: TextView? = null
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
 
+        progressBar = findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tvProgress)
+        tvQuestionTitle = findViewById(R.id.tvQuestionTitle)
+        progressBar = findViewById(R.id.progressBar)
+        ivQuestionImage = findViewById(R.id.ivFlag)
+        tvOption1 = findViewById(R.id.tvOption1)
+        tvOption2 = findViewById(R.id.tvOption2)
+        tvOption3 = findViewById(R.id.tvOption3)
+        tvOption4 = findViewById(R.id.tvOption4)
+
+
         val questionList = Constants.getQuestions()
 
+        var currentPosition = 1
+        val question = questionList[currentPosition - 1]
+        progressBar?.progress = currentPosition
+        tvProgress?.text = "$currentPosition / ${progressBar?.max}"
+        tvQuestionTitle?.text = question.title
+        ivQuestionImage?.setImageResource(question.image)
+        tvOption1?.text = question.option1
+        tvOption2?.text = question.option2
+        tvOption3?.text = question.option3
+        tvOption4?.text = question.option4
     }
 }
